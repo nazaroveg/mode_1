53ef0e24984fbfbcb1cab32d201f371acec5a84a
nazaroveg <nazaroveg@yandex.ru>
2022-07-12 15:29:39 +0300

    v1

---
+++ mode_01.cpp
@@ -0,0 +1,59 @@
+﻿//mode.cpp
+
+#include <iostream>
+
+#define mode '1'    //; не нужно
+
+
+
+#ifdef mode '1'
+
+
+
+void add()
+{
+	std::cout << "Работаю в боевом режиме\n";
+	std::cout << "Введите число 1:\n";
+	int x = 0;
+	std::cin >> x;
+	std::cout << "Введите число 2:\n";
+	int y = 0;
+	std::cin >> y;
+	std::cout << "Результат сложения: " << x + y;
+}
+
+#endif // DEBUG
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+
+
+
+
+    #if mode == '0'
+	{
+		std::cout << "Работаю в режиме тренировки \n";
+	}
+	
+	#elif mode == '1'
+	{
+		add();
+	}
+	#else
+	{
+		std::cout << "Неизвестный режим. Завершение работы \n";
+	}
+
+
+    #endif
+
+
+
+	return 0;
+}
+
+
